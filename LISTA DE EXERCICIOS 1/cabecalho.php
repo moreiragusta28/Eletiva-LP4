ae736319fe923bd1959afac5874bc8acfd4c8732
moreiragusta28 <moreiragusta28@gmail.com>
2025-09-29 16:59:08 -0300

    Cabeçalho lista 01

---
+++ LISTA DE EXERCICIOS 1/cabecalho.php
@@ -0,0 +1,40 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand" href="#">Lista de exercícios 01</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Escolha
+                    </a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown1">
+                        <li><a class="dropdown-item" href="#">Exercício 01</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 02</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 03</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 04</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 05</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 06</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 07</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 08</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 09</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 10</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 11</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 12</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 13</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 14</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 15</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 16</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 17</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 18</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 19</a></li>
+                        <li><a class="dropdown-item" href="#">Exercício 20</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
