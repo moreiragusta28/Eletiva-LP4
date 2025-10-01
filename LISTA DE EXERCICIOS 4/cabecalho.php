b731b20ab899a7779b70a0d57b24860f6cffeb9e
moreiragusta28 <moreiragusta28@gmail.com>
2025-10-01 17:39:29 -0300

    Criando cabeçalho e rodapé

---
+++ LISTA DE EXERCICIOS 4/cabecalho.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Lista de exercícios 04</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="#">Lista de exercícios 04</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Alternar navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="dropdown1" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Escolha
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="dropdown1">
+                            <li><a class="dropdown-item" href="exercicio_01.php">Exercício 01</a></li>
+                            <li><a class="dropdown-item" href="exercicio_02.php">Exercício 02</a></li>
+                            <li><a class="dropdown-item" href="exercicio_03.php">Exercício 03</a></li>
+                            <li><a class="dropdown-item" href="exercicio_04.php">Exercício 04</a></li>
+                            <li><a class="dropdown-item" href="exercicio_05.php">Exercício 05</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <div class="container py-3">
