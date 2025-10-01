59995b1ba2cf9c24fc0f6bf9fceb4878e3682f9e
moreiragusta28 <moreiragusta28@gmail.com>
2025-10-01 16:02:07 -0300

    Exercicio 06

---
+++ LISTA DE EXERCICIOS 2/exercicio_06.php
@@ -0,0 +1,24 @@
+<?php include "cabecalho.php"; ?>
+
+<h1>Exercício 06 - Imprimir usando for</h1>
+<form method="post">
+    <div class="mb-3">
+        <label for="n1" class="form-label">Insira um número</label>
+        <input type="number" id="n1" name="n1" class="form-control" required="">
+    </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $n1 = $_POST["n1"];
+
+    for($i = 1; $i <= $n1; $i++){
+        echo "<p>$i</p>";
+    }
+
+
+}
+
+include "rodape.php";
+?>
