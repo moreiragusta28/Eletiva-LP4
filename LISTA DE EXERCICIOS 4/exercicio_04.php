989b3f69e2455a1212f97fc33bf82c6ed4a10b62
moreiragusta28 <moreiragusta28@gmail.com>
2025-10-02 11:48:07 -0300

    Exercicio 04

---
+++ LISTA DE EXERCICIOS 4/exercicio_04.php
@@ -0,0 +1,45 @@
+<?php include("cabecalho.php"); ?>
+
+<h1>Exercício 4 - Preços com imposto</h1>
+<form method="post">
+    <?php for ($i = 1; $i <= 5; $i++): ?>
+        <p>Dados do Produto <?= $i ?></p>
+        <div class="row inline-row mb-3">
+            <div class="col-md-6">
+                <label for="nome[]" class="form-label">Nome</label>
+                <input type="text" id="nome[]" name="nome[]" class="form-control" required="">
+            </div>
+            <div class="col-md-6">
+                <label for="preco[]" class="form-label">Preço</label>
+                <input type="number" id="preco[]" name="preco[]" class="form-control" step="0.01" required="">
+            </div>
+        </div>
+    <?php endfor; ?>
+    <button type="submit" class="btn btn-primary mt-3">Enviar</button>
+</form>
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $nomes = $_POST['nome'];
+    $precos = $_POST['preco'];
+
+    $produtos = [];
+
+    for ($i = 0; $i < 5; $i++) {
+        $nome = $nomes[$i];
+        $preco = $precos[$i];
+        $preco_final = $preco * 1.15;
+
+        $produtos[$nome] = $preco_final;
+    }
+
+    ksort($produtos);
+
+    echo "<p>Lista de Produtos</p>";
+    foreach ($produtos as $nome => $preco) {
+        echo "<p>Nome: $nome / Preço: R$ " . number_format($preco, 2, ",", ".") ."</p>";
+    }
+}
+
+include("rodape.php");
+?>
