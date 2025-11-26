2b876f1f94ddcc6322820a33839ab528fdb34e31
moreiragusta28 <moreiragusta28@gmail.com>
2025-11-26 14:14:58 -0300

    adequacao novo_cargo

---
+++ PROJETO/nova_cargo.php
@@ -0,0 +1,34 @@
+<?php
+    require("cabecalho.php");
+    require("conexao.php");
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $nome = $_POST['nome'];
+
+        try{
+            $stmt = $pdo->prepare("INSERT INTO cargo (nome) VALUES (?)");
+
+            if($stmt->execute([$nome])){
+                header('location: cargos.php?cadastro=true');
+            } else {
+                header('location: cargos.php?cadastro=false');
+            }
+
+        }catch(Exception $e){
+            echo "Erro: ".$e->getMessage();
+        }
+    }
+?>
+
+<h1>Novo Cargo</h1>
+<form method="post">
+    <div class="mb-3">
+        <label for="nome" class="form-label">Nome do cargo:</label>
+        <input type="text" id="nome" name="nome" class="form-control" required>
+    </div>
+    <button type="submit" class="btn btn-primary">Salvar</button>
+</form>
+
+<?php
+    require("rodape.php");
+?>
