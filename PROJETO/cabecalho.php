00f866f1649ee0681762c3f8782e50802ffbecfd
moreiragusta28 <moreiragusta28@gmail.com>
2025-11-27 13:25:53 -0300

    acrescentando o css no cabecalho

--- PROJETO/cabecalho.php
+++ PROJETO/cabecalho.php
@@ -11,6 +11,7 @@ if (!isset($_SESSION['acesso']))
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Sistema</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/estilo.css">
   <style>
     @media print {
       .no-print {
